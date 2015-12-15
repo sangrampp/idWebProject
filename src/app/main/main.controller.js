433b9e43cb000d25c76d9b409884d7db991d279f
Sangram Powar <sangrampp@gmail.com>
2015-12-15 11:27:54 +0530

    Working instance of carousel for ui-router

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,8 +6,23 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($scope, $timeout, webDevTec, toastr) {
     var vm = this;
 
+    $scope.myInterval = 3000;
+    $scope.noWrapSlides = false;
+    $scope.slides = [];
+    $scope.slides.push({
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    });
   }
 })();
